b8cb2a4aa94646637f415c8f7331d950ead114c8
jszczypka <jszczypka_211i1@e.zseeim.edu.pl>
2024-04-15 09:20:23 +0000

    studentmachine send

---
+++ python/test/main.py
@@ -0,0 +1,14 @@
+print ("witaj swiecie")
+the_world_is_flat = True
+if the_world_is_flat == True:
+	print ("swiat jest plaski")
+else :
+	print ("jest jajkiem")
+x = 5
+y = input("podaj igrek: ")
+print ("igrek wynosi")
+a =int (input("podaj a: "))
+b =int (input("podaj b: "))
+c = a+b
+print (c)
+d = input("podaj y")
