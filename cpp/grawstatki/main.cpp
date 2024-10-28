17c55e28c245474b45fa9f4c7ccbd49f6c76d83b
jszczypka <student@student.machine>
2024-10-28 12:11:17 +0000

    studentmachine send

--- cpp/grawstatki/main.cpp
+++ cpp/grawstatki/main.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 int main(){
+	char test = 64;
+	cout << test;
 	char t[2][2] = {0};
-	int w;
+	int w = 1;
 	int x, y; //współrzedne statka
 	int g, h; //miejsce bombardowania
-	cout << "ile chesz podać statków: ";
-	cin >> w;
+	cout << "ile chesz podać statków(liczba gier): ";
 	for(int i=0;i<w;i++){
 		cout << "gracz 1 podaje współrzędne statku: ";
 		cin >> x >> y;
